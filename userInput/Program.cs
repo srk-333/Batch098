8643e5873043f4b3a8ac983106f32956e11ca5ae
srk-333 <sauravmk333@gmail.com>
2022-01-03 22:53:12 +0530

    [saurav] Added User Input program

---
+++ userInput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace userInput
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your name");
+            String userName = (Console.ReadLine());
+            Console.WriteLine("Enter your age");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(userName);
+            Console.WriteLine(userAge);
+            Console.ReadLine();
+        }
+    }
+}
